006eb58260790ea61c16fe6886c8b74fe9f94871
DmitryCost <70369805+DmitryCost@users.noreply.github.com>
2023-02-19 00:08:13 +0700

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,20 +17,33 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-
-      List<User> users = userService.listUsers();
-      for (User user : users) {
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru")
+              .setCar(new Car("Model1", 11)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru")
+              .setCar(new Car("Model2", 22)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru")
+              .setCar(new Car("Model3", 33)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru")
+              .setCar(new Car("Model4", 44)));
+      try {
+         List<User> users = userService.listUsers();
+         for (User user : users) {
+            System.out.println("Id = "+user.getId());
+            System.out.println("First Name = "+user.getFirstName());
+            System.out.println("Last Name = "+user.getLastName());
+            System.out.println("Email = "+user.getEmail());
+            System.out.println("Car = "+user.getCar());
+         }
+         User user = userService.getUserByCar("Model2", 22);
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+         System.out.println("Car = "+user.getCar());
 
+      } catch (NoResultException e){
+         System.out.println("Not found user" + e);
+      }
       context.close();
    }
 }
